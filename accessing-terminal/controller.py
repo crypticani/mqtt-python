7a63b5b23e625d986184507032e50febfda42291
Aniket <aniket.kumar@kochartech.com>
2022-08-22 19:02:27 +0530

    initial commit

---
+++ accessing-terminal/controller.py
@@ -0,0 +1,89 @@
+##!/usr/bin/env python
+
+
+import paho.mqtt.client as mqtt
+import paho.mqtt.publish as publish
+import random
+import time
+import threading
+import socket,subprocess
+
+
+#######################################
+# -------------------------------------------------
+#
+# MQTT  server details
+# ------------------------------------------------
+#######################################
+broker_addr = "broker.hivemq.com"
+broker_port = 1883
+topic_publish_status = "bd/status"
+toipc_publish_result = "bd/listen"
+topic_subscribe = "bd/result"
+live_interval = 5
+#######################################
+#---------------------------------------------------
+#
+# Class that sends status //MQTT Reciver too
+#
+#---------------------------------------------------
+class run_mqtt(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.daemon = True
+        self.start()
+    
+    def run(self):   
+        client = mqtt.Client()
+        client.on_connect = on_connect
+        client.on_message = on_message
+        try:
+            
+            client.connect(broker_addr, broker_port, 60)
+            
+            # Blocking call that processes network traffic, dispatches callbacks and
+            # handles reconnecting.
+            # Other loop*() functions are available that give a threaded interface and a
+            # manual interface.
+            client.loop_start()
+            
+        except Exception as e:
+            print(e)
+            pass
+      
+def on_connect(client, userdata, flags, rc):
+    #print("Connected with result code "+str(rc))
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    try:
+        client.subscribe(topic_subscribe)
+    except Exception as e:
+        print(e)
+        pass
+    # The callback for when a PUBLISH message is received from the server.
+    
+def on_message(client, userdata, msg):
+    try:
+        message = str(msg.payload.decode("utf-8"))
+        print()
+        print(message)
+        print("------------------------------------------------------\n:", end="")
+        
+    except Exception as e:
+        print(e)
+        pass
+    
+    
+def send(msg):
+    try:
+        publish.single(toipc_publish_result, msg, hostname=broker_addr)
+    except Exception as e:
+        print(e)
+        pass
+    
+run_mqtt()
+print("Welcome to Mqtt-Linux Terminal.")
+while True:
+    data = input(":")
+    send(data)
+    
